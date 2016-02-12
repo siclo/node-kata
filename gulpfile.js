4e70796a0d4c1bc173e7fbf3184dd7ae39e8e7b5
simon.cloutier <simon.cloutier@corp.ovh.com>
2016-02-12 12:04:33 -0500

    Integrated the blink1 reporter

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@
 
 var gulp = require('gulp');
 var $ = require("gulp-load-plugins")();
+var TerminalReporter = require("jasmine-terminal-reporter");
+var Blink1Reporter = require("jasmine-blink1-reporter");
 
 var testFiles = "src/**/*.spec.js";
 
 gulp.task('test', function () {
+    var terminalReporter = new TerminalReporter();
+    var blink1Reporter = new Blink1Reporter();
+    var jasmineOptions = {
+        reporters: [terminalReporter, blink1Reporter]
+    };
     return gulp.src(testFiles)
-        .pipe($.jasmine());
+        .pipe($.jasmine(jasmineOptions));
 });
 
 gulp.task('default', ['test'], function () {
