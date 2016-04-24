398b659a7e92aec13c85c2f6d3aae8883de47033
simon.cloutier <simon.cloutier@corp.ovh.com>
2016-04-24 14:27:23 -0400

    getIps with for loop

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,37 @@
 'use strict';
 
+var servers = [
+    {
+        name: "server1",
+        ip: "10.0.0.1",
+        cpu: 0.1,
+        ram: 0.3
+    },
+    {
+        name: "server2",
+        ip: "10.0.0.2",
+        cpu: 0.9,
+        ram: 0.7
+    },
+    {
+        name: "server3",
+        ip: "10.0.0.3",
+        cpu: 0.0,
+        ram: 0.0
+    }
+    ];
+
+function getIps (servers) {
+    var i;
+    var result = [];
+    for (i = 0; i < servers.length; i++) {
+        result.push(servers[i].ip);
+    }
+    return result;
+}
+
 describe("", function () {
-    it("", function () {
-        expect(1).toEqual(1);
+    it("get the list of ips", function () {
+        expect(getIps(servers)).toEqual(["10.0.0.1", "10.0.0.2", "10.0.0.3"]);
     });
 });
